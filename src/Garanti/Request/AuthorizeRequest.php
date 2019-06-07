33e90a66b205678246ee8a849c75b9adb1a049c0
Murat SAÇ <muratsac@mail.com>
2019-06-07 17:54:58 +0300

    cs fix

--- src/Garanti/Request/AuthorizeRequest.php
+++ src/Garanti/Request/AuthorizeRequest.php
@@ -3,30 +3,33 @@
 namespace Payconn\Garanti\Request;
 
 use Payconn\Common\HttpClient;
+use Payconn\Common\ModelInterface;
 use Payconn\Common\ResponseInterface;
+use Payconn\Common\TokenInterface;
+use Payconn\Garanti\Model\Authorize;
 use Payconn\Garanti\Response\AuthorizeResponse;
+use Payconn\Garanti\Token;
 
 class AuthorizeRequest extends GarantiRequest
 {
+    /**
+     * @return ModelInterface|Authorize
+     */
+    public function getModel(): ModelInterface
+    {
+        return parent::getModel();
+    }
+
+    /**
+     * @return TokenInterface|Token
+     */
+    public function getToken(): TokenInterface
+    {
+        return parent::getToken();
+    }
+
     public function send(): ResponseInterface
     {
-        $securityData = mb_strtoupper(sha1(
-            $this->getToken()->getPassword().
-            '0'.$this->getToken()->getTerminalId()
-        ));
-
-        $hashData = mb_strtoupper(sha1(
-            $this->getToken()->getTerminalId().
-            $this->getModel()->getOrderId().
-            $this->getAmount().
-            $this->getModel()->getSuccessfulUrl().
-            $this->getModel()->getFailureUrl().
-            $this->getModel()->getType().
-            $this->getModel()->getInstallment().
-            $this->getToken()->getStoreKey().
-            $securityData
-        ));
-
         /** @var HttpClient $httpClient */
         $httpClient = $this->getHttpClient();
         $response = $httpClient->request('POST', $this->getModel()->getBaseUrl(), [
@@ -51,12 +54,32 @@ class AuthorizeRequest extends GarantiRequest
                 'errorurl' => $this->getModel()->getFailureUrl(),
                 'customeremailaddress',
                 'customeripaddress' => $this->getIpAddress(),
-                'secure3dhash' => $hashData,
+                'secure3dhash' => $this->getHashData(),
             ],
         ]);
 
         return new AuthorizeResponse($this->getModel(), [
             'content' => $response->getBody()->getContents(),
         ]);
+    }
+
+    private function getHashData(): string
+    {
+        $securityData = mb_strtoupper(sha1(
+            $this->getToken()->getPassword().
+            '0'.$this->getToken()->getTerminalId()
+        ));
+
+        return mb_strtoupper(sha1(
+            $this->getToken()->getTerminalId().
+            $this->getModel()->getOrderId().
+            $this->getAmount().
+            $this->getModel()->getSuccessfulUrl().
+            $this->getModel()->getFailureUrl().
+            $this->getModel()->getType().
+            $this->getModel()->getInstallment().
+            $this->getToken()->getStoreKey().
+            $securityData
+        ));
     }
 }
